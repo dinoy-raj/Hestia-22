61ef53e51529b1bfb3838b06653645ed62957424
Neeraj-SD <neerajsd04@gmail.com>
2022-05-04 10:29:24 +0530

    resized error animation

--- lib/screens/profile/registration_failure.dart
+++ lib/screens/profile/registration_failure.dart
@@ -10,47 +10,51 @@ class RegistrationFailure extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Constants.sc,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Lottie.network(
-                  'https://assets1.lottiefiles.com/packages/lf20_skyzkvon.json'),
-            ),
-            Column(
-              children: const [
-                Text(
-                  'Oops! 😔',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Helvetica',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 24,
-                  ),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Text(
-                  'Something failed.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Helvetica',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Retry'),
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Lottie.network(
+                    'https://assets1.lottiefiles.com/packages/lf20_skyzkvon.json'),
               ),
-            )
-          ],
+              Column(
+                children: const [
+                  Text(
+                    'Oops! 😔',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Helvetica',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    'Something failed.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Helvetica',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Retry'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
