bb54c724389c6e0196b95fa802e09233a371465b
Amal Nath M <amalnathm7@gmail.com>
2022-05-12 19:05:40 +0530

    Data changes

--- lib/screens/spots/cards.dart
+++ lib/screens/spots/cards.dart
@@ -39,171 +39,170 @@ class _CardsState extends State<Cards> {
   @override
   Widget build(BuildContext context) {
     return AnimatedPadding(
-            padding: _animate
-                ? const EdgeInsets.only(
-                    top: 20,
-                  )
-                : const EdgeInsets.only(
-                    top: 20,
+      padding: _animate
+          ? const EdgeInsets.only(
+              top: 20,
+            )
+          : const EdgeInsets.only(
+              top: 20,
+            ),
+      duration: const Duration(milliseconds: 500),
+      child: SizedBox(
+        height: 360,
+        child: NotificationListener(
+          onNotification: (notification) {
+            return true;
+          },
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AnimatedCrossFade(
+                duration: const Duration(milliseconds: 300),
+                firstChild: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 15,
+                    top: 30,
+                    bottom: 30,
                   ),
-            duration: const Duration(milliseconds: 1000),
-            child: SizedBox(
-              height: 360,
-              child: NotificationListener(
-                onNotification: (notification) {
-                  return true;
-                },
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AnimatedCrossFade(
-                      duration: const Duration(milliseconds: 300),
-                      firstChild: Padding(
-                        padding: const EdgeInsets.only(
-                          left: 15,
-                          top: 30,
-                          bottom: 30,
+                  child: RotatedBox(
+                    quarterTurns: -1,
+                    child: AnimatedPadding(
+                      padding: _animate
+                          ? const EdgeInsets.only(
+                              top: 0,
+                            )
+                          : const EdgeInsets.only(
+                              top: 20,
+                            ),
+                      duration: const Duration(milliseconds: 500),
+                      child: const Text(
+                        "The Hotspots",
+                        style: TextStyle(
+                          letterSpacing: 5,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.white24,
                         ),
-                        child: RotatedBox(
-                          quarterTurns: -1,
-                          child: AnimatedPadding(
-                            padding: _animate
-                                ? const EdgeInsets.only(
-                                    top: 0,
-                                  )
-                                : const EdgeInsets.only(
-                                    top: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                secondChild: const SizedBox(
+                  height: 0,
+                ),
+                crossFadeState: _scroll <= 0.2
+                    ? CrossFadeState.showFirst
+                    : CrossFadeState.showSecond,
+              ),
+              Expanded(
+                child: PageView.builder(
+                  controller: _pageController,
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: widget.data.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Hero(
+                      tag: widget.data[index]['title'].toString(),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SpotPage(
+                                          data: widget.data[index],
+                                        )));
+                          },
+                          child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 300),
+                            opacity: _currentPage == index ? 1.0 : 0.5,
+                            child: AnimatedContainer(
+                              duration: const Duration(milliseconds: 300),
+                              padding: const EdgeInsets.all(20),
+                              margin: _currentPage == index
+                                  ? EdgeInsets.zero
+                                  : const EdgeInsets.all(30),
+                              decoration: BoxDecoration(
+                                color: Constants.color3.withOpacity(.25),
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                    opacity: 0.5,
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(widget.data[index]
+                                            ['picture'] ??
+                                        "https://img.collegepravesh.com/2018/10/TKMCE-Kollam.jpg")),
+                              ),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    index + 1 < 10
+                                        ? "0" + (index + 1).toString()
+                                        : (index + 1).toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w300,
+                                        fontSize: 30,
+                                        color:
+                                            Constants.color2.withOpacity(.5)),
                                   ),
-                            duration: const Duration(milliseconds: 1000),
-                            child: const Text(
-                              "The Hotspots",
-                              style: TextStyle(
-                                letterSpacing: 5,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: Colors.white24,
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        widget.data[index]['title'].toString(),
+                                        style: TextStyle(
+                                            overflow: TextOverflow.clip,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                            color: Constants.color2
+                                                .withOpacity(.9)),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        widget.data[index]['desc'].toString(),
+                                        style: TextStyle(
+                                            overflow: TextOverflow.clip,
+                                            fontSize: 14,
+                                            color: Constants.color2
+                                                .withOpacity(.65)),
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text(
+                                        widget.data[index]['short_desc']
+                                            .toString(),
+                                        style: TextStyle(
+                                            overflow: TextOverflow.clip,
+                                            fontSize: 12,
+                                            color: Constants.color2
+                                                .withOpacity(.35)),
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
                             ),
                           ),
                         ),
                       ),
-                      secondChild: const SizedBox(
-                        height: 0,
-                      ),
-                      crossFadeState: _scroll <= 0.2
-                          ? CrossFadeState.showFirst
-                          : CrossFadeState.showSecond,
-                    ),
-                    Expanded(
-                      child: PageView.builder(
-                        controller: _pageController,
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: widget.data.length,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) {
-                          return Hero(
-                            tag: widget.data[index]['title'].toString(),
-                            child: Material(
-                              color: Colors.transparent,
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => SpotPage(
-                                                data: widget.data[index],
-                                              )));
-                                },
-                                child: AnimatedOpacity(
-                                  duration: const Duration(milliseconds: 300),
-                                  opacity: _currentPage == index ? 1.0 : 0.5,
-                                  child: AnimatedContainer(
-                                    duration: const Duration(milliseconds: 300),
-                                    padding: const EdgeInsets.all(20),
-                                    margin: _currentPage == index
-                                        ? EdgeInsets.zero
-                                        : const EdgeInsets.all(30),
-                                    decoration: BoxDecoration(
-                                      color: Constants.color3.withOpacity(.25),
-                                      borderRadius: BorderRadius.circular(20),
-                                      image: DecorationImage(
-                                          opacity: 0.5,
-                                          fit: BoxFit.cover,
-                                          image: NetworkImage(widget.data[index]
-                                                  ['picture'] ??
-                                              "https://img.collegepravesh.com/2018/10/TKMCE-Kollam.jpg")),
-                                    ),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          index + 1 < 10
-                                              ? "0" + (index + 1).toString()
-                                              : (index + 1).toString(),
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w300,
-                                              fontSize: 30,
-                                              color: Constants.color2
-                                                  .withOpacity(.5)),
-                                        ),
-                                        Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              widget.data[index]['title'].toString(),
-                                              style: TextStyle(
-                                                  overflow: TextOverflow.clip,
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 20,
-                                                  color: Constants.color2
-                                                      .withOpacity(.9)),
-                                            ),
-                                            const SizedBox(
-                                              height: 5,
-                                            ),
-                                            Text(
-                                              widget.data[index]['desc'].toString(),
-                                              style: TextStyle(
-                                                  overflow: TextOverflow.clip,
-                                                  fontSize: 14,
-                                                  color: Constants.color2
-                                                      .withOpacity(.65)),
-                                            ),
-                                            const SizedBox(
-                                              height: 10,
-                                            ),
-                                            Text(
-                                              widget.data[index]['short_desc']
-                                                  .toString(),
-                                              style: TextStyle(
-                                                  overflow: TextOverflow.clip,
-                                                  fontSize: 12,
-                                                  color: Constants.color2
-                                                      .withOpacity(.35)),
-                                            ),
-                                            const SizedBox(
-                                              height: 10,
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
               ),
-            ),
-          );
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
