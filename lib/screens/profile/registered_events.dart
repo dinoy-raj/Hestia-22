97eb15888528e87ab5919e94ba76f55e40574f83
Amal Nath M <amalnathm7@gmail.com>
2022-05-13 15:35:18 +0530

    Events

--- lib/screens/profile/registered_events.dart
+++ lib/screens/profile/registered_events.dart
@@ -1,8 +1,7 @@
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hestia22/main.dart';
+import 'package:hestia22/screens/events/events.dart';
 
 class RegisteredEvents extends StatefulWidget {
   const RegisteredEvents({Key? key}) : super(key: key);
@@ -81,92 +80,101 @@ class _RegisteredEventsState extends State<RegisteredEvents> {
                       : ListView.separated(
                           shrinkWrap: true,
                           itemBuilder: (context, index) {
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 18),
-                              child: AnimatedOpacity(
-                                duration: const Duration(milliseconds: 300),
-                                opacity: start ? 1 : 0.2,
-                                child: AnimatedContainer(
-                                  height: size.width * 0.5,
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            EventDetails(data![index])));
+                              },
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 18),
+                                child: AnimatedOpacity(
                                   duration: const Duration(milliseconds: 300),
-                                  padding: const EdgeInsets.all(20),
-                                  margin: start
-                                      ? EdgeInsets.zero
-                                      : const EdgeInsets.all(30),
-                                  decoration: BoxDecoration(
-                                    color: Constants.iconAc.withOpacity(.25),
-                                    borderRadius: BorderRadius.circular(15),
-                                    image: DecorationImage(
-                                      opacity: 0.5,
-                                      fit: BoxFit.cover,
-                                      image:
-                                          NetworkImage(data![index]['image']),
+                                  opacity: start ? 1 : 0.2,
+                                  child: AnimatedContainer(
+                                    height: size.width * 0.5,
+                                    duration: const Duration(milliseconds: 300),
+                                    padding: const EdgeInsets.all(20),
+                                    margin: start
+                                        ? EdgeInsets.zero
+                                        : const EdgeInsets.all(30),
+                                    decoration: BoxDecoration(
+                                      color: Constants.iconAc.withOpacity(.25),
+                                      borderRadius: BorderRadius.circular(15),
+                                      image: DecorationImage(
+                                        opacity: 0.5,
+                                        fit: BoxFit.cover,
+                                        image:
+                                            NetworkImage(data![index]['image']),
+                                      ),
                                     ),
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        data![index]['title'],
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 25,
-                                            color: Constants.color2
-                                                .withOpacity(.65)),
-                                      ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Expanded(
-                                            child: Text(
-                                              data![index]['start_date'] ??
-                                                  "May 26-29",
-                                              textAlign: TextAlign.center,
-                                              style: TextStyle(
-                                                  overflow: TextOverflow.clip,
-                                                  fontSize: 15,
-                                                  color: Constants.color2
-                                                      .withOpacity(.5)),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          data![index]['title'],
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 25,
+                                              color: Constants.color2
+                                                  .withOpacity(.65)),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Expanded(
+                                              child: Text(
+                                                data![index]['start_date'] ??
+                                                    "May 26-29",
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                    overflow: TextOverflow.clip,
+                                                    fontSize: 15,
+                                                    color: Constants.color2
+                                                        .withOpacity(.5)),
+                                              ),
                                             ),
-                                          ),
-                                          const SizedBox(
-                                            width: 5,
-                                          ),
-                                          Expanded(
-                                            child: Text(
-                                              data![index]['short_desc']
-                                                  .toString(),
-                                              textAlign: TextAlign.center,
-                                              style: TextStyle(
-                                                  overflow: TextOverflow.clip,
-                                                  fontSize: 15,
-                                                  color: Constants.color2
-                                                      .withOpacity(.75)),
+                                            const SizedBox(
+                                              width: 5,
                                             ),
-                                          ),
-                                          const SizedBox(
-                                            width: 5,
-                                          ),
-                                          Expanded(
-                                            child: Text(
-                                              data![index]['venue']['title']
-                                                  .toString(),
-                                              textAlign: TextAlign.center,
-                                              style: TextStyle(
-                                                  overflow: TextOverflow.clip,
-                                                  fontSize: 15,
-                                                  color: Constants.color2
-                                                      .withOpacity(.5)),
+                                            Expanded(
+                                              child: Text(
+                                                data![index]['short_desc']
+                                                    .toString(),
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                    overflow: TextOverflow.clip,
+                                                    fontSize: 15,
+                                                    color: Constants.color2
+                                                        .withOpacity(.75)),
+                                              ),
                                             ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
+                                            const SizedBox(
+                                              width: 5,
+                                            ),
+                                            Expanded(
+                                              child: Text(
+                                                data![index]['venue']['title']
+                                                    .toString(),
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                    overflow: TextOverflow.clip,
+                                                    fontSize: 15,
+                                                    color: Constants.color2
+                                                        .withOpacity(.5)),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
