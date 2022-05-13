8a622946ca75ca159ff6a275080ec3631433a9e7
Adarsh S Kumar <adarsh@Admins-MacBook-Air.local>
2022-05-13 20:32:34 +0530

    time

--- lib/screens/schedule/dates/first.dart
+++ lib/screens/schedule/dates/first.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:hestia22/services/django/django.dart' as django;
 import 'package:flutter/material.dart';
 import 'package:hestia22/screens/schedule/schedule_screen.dart';
@@ -14,6 +16,8 @@ class FirstPage extends StatefulWidget {
 class _FirstPageState extends State<FirstPage> {
   bool start = false;
   List<dynamic>? data;
+  DateTime? startDateFormat;
+  DateTime? endDateFormat;
 
   @override
   void initState() {
@@ -21,9 +25,14 @@ class _FirstPageState extends State<FirstPage> {
     super.initState();
     django.getSchedule("2022-05-26").then((value) {
       setState(() {
-        
+
       data = value;
       });});
+
+    //   startDateFormat =
+    //     DateTime.parse(data![0]['event_start']);
+    // DateTime startDate = startDateFormat!.toLocal();
+    // log(startDate.toString());
 
     Future.delayed(const Duration(milliseconds: 200), () {
       setState(() {
@@ -44,6 +53,7 @@ class _FirstPageState extends State<FirstPage> {
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
         child: data==null? const Center(child: CircularProgressIndicator()): ListView.builder(
           itemCount: data!.length,
+          
           itemBuilder: ((context, index) {
           return SizedBox(
               height: size.width * 0.39,
@@ -54,7 +64,7 @@ class _FirstPageState extends State<FirstPage> {
                     width: size.width * 0.05
                   ),
                   EventCard(
-                    time: '08:00 - 10:00',
+                    time: '08:00 - 8.30',
                     eventName: data![index]['title'].toString(),
                     description: data![index]['venue']['title'].toString(),
                   ),
@@ -67,3 +77,4 @@ class _FirstPageState extends State<FirstPage> {
     );
   }
 }
+
